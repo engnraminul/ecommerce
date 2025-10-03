2e08f478c0e6fa78c4686254162c6ad3b965d49f
engnraminul <engnraminul@gmail.com>
2025-10-04 01:27:18 +0600

    fix loading problem in settings

--- test_view.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def test_checkout_template(request):
-    """Test view to render checkout template directly"""
-    return render(request, 'frontend/checkout.html', {
-        'cart': None,  # Empty cart for testing
-        'total_amount': 0,
-    })
